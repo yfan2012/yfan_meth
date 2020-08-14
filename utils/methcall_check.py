72744cdbffed5164f0bf1fc788792ee39646f57b
yfan2012 <yfan2012@gmail.com>
2020-08-13 22:15:10 -0400

    switching to read-based motifs

--- utils/methcall_check.py
+++ utils/methcall_check.py
@@ -42,6 +42,12 @@ def fasta_dict(reffile):
     fastadict={ x:fa.fetch(x) for x in tigs }
     return fastadict
 
+def rev_comp(seq):
+    key={'A': 'T', 'T':'A', 'G':'C',  'C':'G'}
+    newseq=''
+    for i in seq:
+        newseq+=key[i]
+    return newseq[::-1]
 
 def motif_position(fastadict, motifs, modpos):
     '''
@@ -73,52 +79,53 @@ def read_mods(fast5, bamfile, motifpos, motifs, modpos, q):
 
     motiflen=len(motifs[0])
 
-    
     with get_fast5_file(fast5, mode="r") as f5:
         for read_id in f5.get_read_ids():
             readff=f5.get_read(read_id)
             latest_basecall=readff.get_latest_analysis('Basecall_1D')
             mod_base_table=readff.get_analysis_dataset(latest_basecall, 'BaseCalled_template/ModBaseProbs')
             readrecord=baminfo.find(read_id)
-
+            
             for read in readrecord:
-
                 ##make sure the record is not unmapped or secondary (calmd doesn't give these md tags)
                 if not read.is_unmapped and not read.is_secondary:
                     align=np.array(read.get_aligned_pairs(with_seq=True))
                     chrom=read.reference_name
-                    seq=read.get_forward_sequence()
-
+                    if read.is_reverse:
+                        seq=rev_comp(read.get_forward_sequence())
+                        strand='-'
+                    else:
+                        seq=read.get_forward_sequence()
+                        strand='+'
                     minrefpos=read.reference_start
                     maxrefpos=read.reference_end
                     firstmotif=sum(motifpos[chrom]<minrefpos)+1
                     lastmotif=sum(motifpos[chrom]<maxrefpos)
-
+                    
                     ###go through each motif position in the alignemnt range (so you're not scanning thru the whole genome)
                     for pos in motifpos[chrom][firstmotif:lastmotif]:
                         alignidx=np.where(align[:,1]==pos)[0][0]
                         alignpos=align[alignidx]
 
-                        ##check for recognizable motif on the read, assume uniform length motifs for now
-                        startidx=int(alignidx-modpos)
-                        endidx=int(startidx+motiflen)
-                        alignmotif=align[int(startidx):int(endidx)][:,2]
-                        if sum(alignmotif==None)>0:
-                            motif_correct='del'
+                        ##if the mod position is a deletion, all bets are off
+                        if alignpos[0] == None:
+                            q.put(','.join(map(lambda x: str(x), [read_id, 'del', '-', chrom, alignpos[1], alignpos[2], 'del', 'del', 'False', strand]))+'\n')
+
                         else:
-                            readmotif=''.join(alignmotif)
+                            ##check for recognizable motif on the read, assume uniform length motifs for now
+                            startidx=alignpos[0]-modpos
+                            endidx=alignpos[0]-modpos+motiflen
+                            readmotif=seq[startidx:endidx]
                             if readmotif in motifs:
                                 motif_correct='True'
                             else:
                                 motif_correct='False'
-
-                        ##report mod probs if relevant
-                        if alignpos[0]==None:
-                            q.put(','.join(map(lambda x: str(x), [read_id, 'del', 'del', chrom, alignpos[1], alignpos[2], 'del', 'del', motif_correct]))+'\n')
-                        else:
-                            modprobs=mod_base_table[alignpos[0]]
-                            q.put(','.join(map(lambda x: str(x), [read_id, alignpos[0], seq[alignpos[0]], chrom, alignpos[1], alignpos[2], modprobs[1], modprobs[3], motif_correct]))+'\n')
-                            
+                                                
+                            if read.is_reverse:
+                                modprobs=mod_base_table[len(seq)-alignpos[0]-motiflen+modpos+modpos]
+                            else:
+                                modprobs=mod_base_table[alignpos[0]]
+                            q.put(','.join(map(lambda x: str(x), [read_id, alignpos[0], seq[alignpos[0]], chrom, alignpos[1], alignpos[2], modprobs[1], modprobs[3], motif_correct, strand]))+'\n')
 
                         
     print(fast5+' took '+str(time.time()-start_time) + ' seconds')
