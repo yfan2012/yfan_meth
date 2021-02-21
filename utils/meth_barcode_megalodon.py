ad6538c4f6e8adbfbd46a2fd81247e66d39f8ec7
yfan2012 <yfan2012@gmail.com>
2021-02-21 18:51:47 -0500

    index megalodon output for parallel barcodes later

--- utils/meth_barcode_megalodon.py
+++ utils/meth_barcode_megalodon.py
@@ -1,6 +1,16 @@
 import argparse
-import multiporcessing
+import multiprocessing
 import pysam
 
 def parseArgs():
+    parser=argparse.ArgumentParser(description='get a meth barcode per read for megalodon')
+    parser.add_argument('-i', '--input', type=str, required=True, help='megalodon output file with mod motif probs')
+    parser.add_argument('-r', '--ref', type=str, required=True, help='reference genome used in megaldon')
+    parser.add_argument('-m', '--motif', type=str, required=True, help='motif list file')
+    parser.add_argument('-o', '--out', type=str, required=True, help='output file that lists each read and each barcode number')
+    parser.add_argument('-t', '--threads', type=int, required=True, help='number of threads to use')
+    parser.add_argument('-l', '--numreads', type=int, required=False, help='how many reads to consider')
+    args=parser.parse_args()
+    return args
+
 
