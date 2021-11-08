4de0e48fc391ee1643e131a4a307f44e5952e1cc
yfan2012 <yfan2012@gmail.com>
2021-11-07 23:40:02 -0500

    more messing around with parallel methods

--- utils/megalodon_agg_experimental.py
+++ utils/megalodon_agg_experimental.py
@@ -1,15 +1,13 @@
-from functools import partial
-import sys
 import itertools
 import argparse
 import multiprocessing as mp
 import pysam
 import math
+import sys
 import numpy as np
 from itertools import repeat
 from megalodon_barcode_functions import *
 import time
-from joblib import Parallel, delayed 
 
 def parseArgs():
     parser=argparse.ArgumentParser(description='get a meth barcode per read for megalodon')
@@ -22,174 +20,201 @@ def parseArgs():
     args=parser.parse_args()
     return args
 
-##try nanocompore parallel method
 
-class methylation_summary ():
-    from megalodon_barcode_functions import fasta_dict, revcomp, find_thresh
-    import time
-    def __init__(self,
-                 modfile:str,
-                 idxfile:str,
-                 reffile:str,
-                 outfile:str,
-                 threads:int=12,
-                 verbose:bool=False):
-        self.__modfile=modfile
-        self.__idxfile=idxfile
-        self.__reffile=reffile
-        self.__outfile=outfile
-        self.__threads=threads
-        self.__verbose=verbose
-        self.__ref=fasta_dict(reffile)
+def get_empty_ref(ref):
+    '''
+    take ref dict
+    return ref dict with np array full of zeros
+    '''
+    aggcalls={}
+    for i in ref:
+        aggcalls[i]=np.zeros(len(ref[i]))
+    return aggcalls
 
-        
-    def __call__(self):
-        in_q=mp.Queue() ##full of read batches
-        out_q=mp.Queue() ##full of (aggmeth, aggunmeth)
-        err_q=mp.Queue()
-        
-        
-        processes=[]
-        processes.append(mp.Process(target=self.__get_reads, args=(in_q, err_q)))
-        for i in range(self.__threads):
-            processes.append(mp.Process(target=self.__aggregate_reads, args=(in_q, out_q)))
-        processes.append(mp.Process(target=self.__write_reads, args=(out_q, err_q)))
 
-        for p in processes:
-            p.start()
+def get_strand(ref):
+    '''
+    take the refernce 
+    return strand info
+    '''
+    strands=get_empty_ref(ref)
+    for i in ref:
+        for j in range(len(ref[i])):
+            if ref[i][j]=='A' or ref[i][j]=='C':
+                strands[i][j]+=1
+    return strands
 
-        for p in processes:
-            p.join()
-        
-        in_q.close()
-        out_q.close()
-
-        
-    def __get_reads(self, in_q, err_q):
-        '''
-        take chunks of read info from readidx
-        return batch of reads
-        '''
-        threads=self.__threads
-        ref=self.__ref
-        readidx=read_megalodon_index(self.__idxfile)
-
-        ##separate reads into batches
-        idxchunks=[]
-        chunksize=math.ceil(len(readidx)/threads/100)
-        for i in range(0,threads*100):
-            start=i*chunksize
-            end=(i+1)*chunksize
-            if len(readidx)<end:
-                idxchunks.append(readidx[start:])
-            else:
-                idxchunks.append(readidx[start:end])
-
-        ##read in the read batch
-        for readinfo in idxchunks:
-            if len(readinfo)>0:
-                startbyte=readinfo[0][2]
-                endbyte=readinfo[-1][2]+readinfo[-1][3]
-                bytelen=endbyte-startbyte
-                readbatch=[]
-                with open(self.__modfile, 'r') as f:
-                    f.seek(startbyte,0)
-                    readcontent=f.read(bytelen).split('\n')
-                    f.close()
-                for i in readcontent:
-                    readbatch.append(i.split('\t'))
-                in_q.put(readbatch)
-        for i in range(self.__threads):
-            in_q.put(None)
-
-            
-    def __aggregate_reads(self, in_q, out_q):
-        '''
-        take a batch of reads (in idxchunk)
-        return aggregated read methylation calls
-        '''
-        for readbatch in iter(in_q.get, None):
-            '''
-            print('starting batch of length %d ' % len(readbatch))
-            sys.stdout.flush()
-            tstart=time.time()
-            '''
-            aggmeth=self.__get_empty_ref()
-            aggunmeth=self.__get_empty_ref()
-            thresh=find_thresh()
-            
-            for i in readbatch[0:-1]:
-                ratio=math.log10(float(i[5])/float(i[4]))
-                pos=float(i[3])
-                base=self.__ref[i[1]][int(i[3])]
-                if base=='C' or base=='G':
-                    if ratio > thresh[0]:
-                        aggmeth[i[1]][int(i[3])]+=1
-                    else:
-                        aggunmeth[i[1]][int(i[3])]+=1
-                if base=='A' or base=='T':
-                    if ratio > thresh[1]:
-                        aggmeth[i[1]][int(i[3])]+=1
-                    else:
-                        aggunmeth[i[1]][int(i[3])]+=1
-            out_q.put((aggmeth, aggunmeth))
-        out_q.put(None)
-        
-    def __get_empty_ref(self):
-        '''
-        take ref dict
-        return ref dict with np array full of zeros
-        '''
-        aggcalls={}
-        for i in self.__ref:
-            aggcalls[i]=np.zeros(len(self.__ref[i]))
-        return aggcalls
-
+def get_reads(idxchunks, q, threads):
+    '''
+    take read info from read index
+    return batch of reads
+    '''
+    for readinfo in idxchunks:
+        startbyte=readinfo[0][2]
+        endbyte=readinfo[-1][2]+readinfo[-1][3]
+        bytelen=endbyte-startbyte
+        readbatch=[]
+        with open(modfile, 'r') as f:
+            f.seek(startbyte,0)
+            readcontent=f.read(bytelen).split('\n')
+            f.close()
+        for i in readcontent:
+            readbatch.append(i.split('\t'))
+        q.put(readbatch)
+    for i in range(threads):
+        q.put('done now')
     
-
-    def __write_reads(self, out_q, err_q):
-        '''
-        write from the out queue
-        '''
         
-        methagg=[]
-        unmethagg=[]
-        for _ in range(self.__threads):
-            for am, au in iter(out_q.get, None):
-                methagg.append(am)
-                unmethagg.append(au)
-                
-        with open (self.__outfile, 'w') as f:
-                fullmeth=self.__sum_refs(methagg, self.__ref)
-                fullunmeth=self.__sum_refs(unmethagg, self.__ref)
-                for i in fullmeth:
-                    for pos in range(0, len(fullmeth[i])):
-                        towrite=[i, str(pos), str(fullmeth[i][pos]), str(fullunmeth[i][pos])]
-                        f.write('\t'.join(towrite)+'\n')
+def aggregate_reads(q, r):
+    '''
+    take a batch of reads (in idxchunk)
+    return aggregated read methylation calls
+    '''
+    while True:
+        readbatch=q.get()
+        if readbatch=='done now':
+            r.put('done now')
+            break
+        
+        aggmeth=get_empty_ref(ref)
+        aggunmeth=get_empty_ref(ref)
+
+        for i in readbatch[0:-1]:
+            ratio=math.log10(float(i[5])/float(i[4]))
+            pos=float(i[3])
+            base=ref[i[1]][int(i[3])]
+            if base=='C' or base=='G':
+                if ratio > thresh[0]:
+                    aggmeth[i[1]][int(i[3])]+=1
+                else:
+                    aggunmeth[i[1]][int(i[3])]+=1
+            if base=='A' or base=='T':
+                if ratio > thresh[1]:
+                    aggmeth[i[1]][int(i[3])]+=1
+                else:
+                    aggunmeth[i[1]][int(i[3])]+=1
+        r.put([aggmeth, aggunmeth])
+    r.put('done now')
 
 
-    def __sum_refs(aglist):
-        '''
-        take list of aggregated meth calls
-        combine them into one final thing
-        '''
-        fullmeth=get_empty_ref()
-        for i in aglist:
-            for j in i:
-                fullmeth[j]=np.add(i[j], fullmeth[j])
-        return fullmeth
-                 
+def gather_results(r, aggmeth, aggunmeth, threads):
+    '''
+    get results from queue and put them in lists
+    '''
+    dones=0
+    while True:
+        res=r.get()
+        if res == 'done now':
+            dones+=1
+            if dones==threads:
+                break
+        else:
+            aggmeth.append(res[0])
+            aggunmeth.append(res[1])
+    return aggmeth, aggunmeth
 
 
-def main(reffile, modfile, idxfile, outfile, threads, verbose):
-    m=methylation_summary(modfile=modfile,
-                        reffile=reffile,
-                        idxfile=idxfile,
-                        outfile=outfile,
-                        threads=threads,
-                        verbose=verbose)
-    m()
+def sum_refs(agg):
+    '''
+    take list of aggregated meth calls
+    combine them into one final thing
+    '''
+    fullmeth=get_empty_ref(ref)
+    for i in agg:
+        for chrom in fullmeth:
+                fullmeth[chrom]=np.add(i[chrom], fullmeth[chrom])
+    return fullmeth
+
+
 
 if __name__ == "__main__":
     args=parseArgs()
-    main(args.reffile, args.modfile, args.idxfile, args.outfile, args.threads, args.verbose)
+    reffile=args.reffile
+    modfile=args.modfile
+    idxfile=args.idxfile
+    threads=args.threads
+    verbose=args.verbose
+
+    start_time=time.time()
+    if verbose:
+        print('reading reference and index files, splitting into jobs')
+    ref=fasta_dict(reffile)
+    strands=get_strand(ref)
+    readidx=read_megalodon_index(idxfile)
+    ##potentially add custom thresholds later
+    thresh=find_thresh()
+
+
+    
+    ##split idx into chunks
+    idxchunks=[]
+    chunksize=3000
+    numchunks=math.ceil(len(readidx)/3000)
+    for i in range(numchunks):
+        start=i*chunksize
+        end=(i+1)*chunksize
+        if len(readidx)<end:
+            if len(readidx[start:]) > 0:
+                idxchunks.append(readidx[start:])
+        else:
+            if len(readidx[start:end]) > 0:
+                idxchunks.append(readidx[start:end])
+
+
+                
+    if verbose:
+        read_duration=round(time.time()-start_time)
+        print('finished reading in %d seconds' % (read_duration))
+        print('starting parallel')
+        par_start=time.time()
+
+
+        
+    manager=mp.Manager()
+    q=manager.Queue()
+    r=manager.Queue()
+    methagg=[]
+    unmethagg=[]
+
+    processes=[]
+    processes.append(mp.Process(target=get_reads, args=(idxchunks, q, threads)))
+    for i in range(threads):
+        processes.append(mp.Process(target=aggregate_reads, args=(q, r)))
+    processes.append(mp.Process(target=gather_results, args=(r, methagg, unmethagg, threads)))
+
+    for process in processes:
+        process.start()
+
+    for process in processes:
+        process.join()
+
+    q.close()
+    r.close()
+
+
+        
+    if verbose:
+        par_duration=round(time.time()-par_start)
+        print('finished parallel in %d seconds' % (par_duration))
+        print('starting aggregation')
+        agg_start=time.time()
+
+    fullmeth=sum_refs(methagg)
+    fullunmeth=sum_refs(unmethagg)
+
+    if verbose:
+        agg_duration=round(time.time()-agg_start)
+        print('finished aggregation in %d seconds' % (agg_duration))
+        print('starting writing')
+        write_start=time.time()
+        
+
+    with open (outfile, 'w') as f:
+        for i in fullmeth:
+            for pos in range(0, len(fullmeth[i])):
+                towrite=[i, str(pos), str(strands[i][j]), str(fullmeth[i][pos]), str(fullunmeth[i][pos])]
+                f.write('\t'.join(towrite)+'\n')
+
+    if verbose:
+        write_duration=round(time.time()-write_start)
+        print('finished writing in %d seconds' % (write_duration))
